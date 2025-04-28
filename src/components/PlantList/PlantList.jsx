167d44a9d3e7081891f22f2ca2047943de3758b5
Martinus Aamot Dahl <maraad97@gmail.com>
2025-04-28 14:43:18 +0200

    Create render plants component

--- src/components/PlantList/PlantList.jsx
+++ src/components/PlantList/PlantList.jsx
@@ -1,7 +1,24 @@
 import styles from "./PlantList.module.css";
 
-const PlantList = () => {
-  return <div>PlantList</div>;
+const PlantList = ({ listArray }) => {
+  return (
+    <div className={styles.rootContainer}>
+      {listArray.map((item) => {
+        return (
+          <div className={styles.cardContainer}>
+            <img
+              src={item.image}
+              alt={`Image of ${item.name}`}
+              className={styles.plantImage}
+            />
+            <div className={styles.cardInfoContainer}>
+              <h3 className={styles.plantName}>{item.name}</h3>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default PlantList;
