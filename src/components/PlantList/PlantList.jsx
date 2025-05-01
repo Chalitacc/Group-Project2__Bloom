7be75ac9c450ac6e7cd57f2c2b04996cb27e7c19
Martinus Aamot Dahl <maraad97@gmail.com>
2025-05-01 18:31:29 +0200

    Moved single plant render from PlantList to PlantItem, and added backside container

--- src/components/PlantList/PlantList.jsx
+++ src/components/PlantList/PlantList.jsx
@@ -1,21 +1,11 @@
+import PlantItem from "../PlantItem/PlantItem";
 import styles from "./PlantList.module.css";
 
 const PlantList = ({ listArray }) => {
   return (
     <div className={styles.rootContainer}>
       {listArray.map((item) => {
-        return (
-          <div className={styles.cardContainer}>
-            <img
-              src={item.image}
-              alt={`Image of ${item.name}`}
-              className={styles.plantImage}
-            />
-            <div className={styles.cardInfoContainer}>
-              <h3 className={styles.plantName}>{item.name}</h3>
-            </div>
-          </div>
-        );
+        return <PlantItem item={item} />;
       })}
     </div>
   );
